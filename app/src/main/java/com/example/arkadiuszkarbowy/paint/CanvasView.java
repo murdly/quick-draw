f3d838defb6f54c318c41a5d29bc9631f20433d4
murdly <fretowski@gmail.com>
2015-09-09 12:03:03 +0200

    layout

---
+++ app/src/main/java/com/example/arkadiuszkarbowy/paint/CanvasView.java
@@ -0,0 +1,16 @@
+package com.example.arkadiuszkarbowy.paint;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by arkadiuszkarbowy on 09/09/15.
+ */
+public class CanvasView extends View {
+
+    public CanvasView(Context context, AttributeSet attr){
+        super(context, attr);
+
+    }
+}
