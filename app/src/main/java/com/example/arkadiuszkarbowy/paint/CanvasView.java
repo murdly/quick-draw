fc0038f9246372984b35971bdd04c35cc1e13ce2
murdly <fretowski@gmail.com>
2015-09-09 16:53:11 +0200

    toolbar, landscape, rysowanie

--- app/src/main/java/com/example/arkadiuszkarbowy/paint/CanvasView.java
+++ app/src/main/java/com/example/arkadiuszkarbowy/paint/CanvasView.java
@@ -1,7 +1,12 @@
 package com.example.arkadiuszkarbowy.paint;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -9,8 +14,65 @@ import android.view.View;
  */
 public class CanvasView extends View {
 
-    public CanvasView(Context context, AttributeSet attr){
+    private static final int DEFAULT_COLOR = 0xFF000000;
+    private Path mPath;
+    private Paint mDrawPaint, mCanvasPaint;
+    private int mPaintColor;
+    private Canvas mCanvas;
+    private Bitmap mCanvasBitmap;
+
+    public CanvasView(Context context, AttributeSet attr) {
         super(context, attr);
 
+        setUp();
+    }
+
+    private void setUp() {
+        mPath = new Path();
+        mCanvasPaint = new Paint(Paint.DITHER_FLAG);
+        mDrawPaint = new Paint();
+        mDrawPaint.setColor(DEFAULT_COLOR);
+        mDrawPaint.setStrokeWidth(20);
+        mDrawPaint.setStrokeJoin(Paint.Join.ROUND);
+        mDrawPaint.setStrokeCap(Paint.Cap.ROUND);
+        mDrawPaint.setStyle(Paint.Style.STROKE);
+        mDrawPaint.setAntiAlias(true);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mCanvasBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(mCanvasBitmap, 0, 0, mCanvasPaint);
+        canvas.drawPath(mPath, mDrawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        super.onTouchEvent(event);
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mPath.moveTo(event.getX(), event.getY());
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mPath.lineTo(event.getX(), event.getY());
+                break;
+            case MotionEvent.ACTION_UP:
+                mCanvas.drawPath(mPath, mDrawPaint);
+                mPath.reset();
+                break;
+            default:
+                return false;
+        }
+        invalidate();
+        return true;
     }
 }
--- app/src/main/java/com/example/arkadiuszkarbowy/paint/MainActivity.java
+++ app/src/main/java/com/example/arkadiuszkarbowy/paint/MainActivity.java
@@ -2,34 +2,47 @@ package com.example.arkadiuszkarbowy.paint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mTitle;
+    private String mDefaultTitle;
+    private CanvasView mCanvasView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.menu_main);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        mDefaultTitle = getString(R.string.title);
+        mTitle = (EditText)findViewById(R.id.title);
+        mTitle.setText(mDefaultTitle);
+        mTitle.setSelection(mTitle.getText().length());
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_open_file) {
+            return true;
+        } else if (id == R.id.action_save_file) {
             return true;
         }
 
